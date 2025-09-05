d8db383671a7e57032978f6d44fcb3ccf5d6e894
rahulsengupta0 <rahul@creditoracademy.com>
2025-09-05 11:14:58 +0530

    ready section

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -3,9 +3,11 @@ import Hero from '../../components/homepage/hero';
 import About from '../../components/homepage/about';
 // import AboutHero from '../../components/homepage/AboutHero';
 // import AboutFeatures from '../../components/homepage/AboutFeatures';
-// import Ready from '../../components/Ready.jsx';
+import Ready from '../../components/homepage/readysection';
 import Features from '../../components/homepage/features';
-import LearningSection from '../../components/homepage/learningSection';
+import LearningSection from '../../components/homepage/learningsection';
+
+
 
 const Home = () => {
   return (
@@ -14,7 +16,7 @@ const Home = () => {
       <About/>
       <Features />
       <LearningSection />
-      {/* <Ready /> */}
+      <Ready />
     </>
   );
 };
